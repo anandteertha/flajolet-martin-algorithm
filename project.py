0b3d9c2fc9e1b047aedd6204acd941b754a2bffc
anandteertha <anandteertha.ramesh18@siesgst.ac.in>
2021-10-24 19:31:54 +0530

    stream data

--- project.py
+++ project.py
@@ -1,7 +1,27 @@
+import requests
 class FlajoletMartin:
-    def __init__(self,dataStream,hash):
-        self.dataStream = dataStream
+    def __init__(self,hash,hashes):
+        #self.dataStream = dataStream
         self.hash = hash
+        self.hashes = hashes
+        self.groupSize = int(input('enter the grouping size\n'))
+        self.startStream()
+    def convertImg2listOfpixels(self,image):
+        l = [x for x in image]
+        return l
+    def startStream(self):
+        url = input('enter the url to get video stream:- \n')
+        while True:
+            try:
+                image = requests.get(url)
+                image = self.convertImg2listOfpixels(image.content)
+                self.dataStream = image
+                self.runFMAlgorithm()
+                self.extendedFMAlgorithm()
+                self.extendedFMAlgorithmMedian()
+                self.combineApproach()
+            except KeyboardInterrupt:
+                break
     def average(self,lists):
         s = sum(lists)
         return int(s/len(lists))
@@ -36,24 +56,11 @@ class FlajoletMartin:
             n = self.getTrailingZeros(s)
             self.trailingZeros.append(n)
         r = max(self.trailingZeros)
-        print('*****NORMAL FM ALGORITHM*****\n')
+        print('\n*****NORMAL FM ALGORITHM*****')
         print('r = ',r)
         print('R(number of distinct elements) =',2**r)
 
     def extendedFMAlgorithm(self):
-        self.hashes = []
-        tmp = [int(m) for m in self.hash]
-        self.hashes.append(tmp)
-        self.no_of_hash = int(input('enter the number of hash you require?\n'))
-        for i in range(1,self.no_of_hash):
-            if i+1 == 2:
-                tmp = input('enter the '+str(i+1)+'nd hash coefficients\n' ).strip().split()
-            elif i+1 == 3:
-                tmp = input('enter the '+str(i+1)+'rd hash coefficients\n' ).strip().split()
-            else:
-                tmp = input('enter the '+str(i+1)+'th hash coefficients\n' ).strip().split()
-            tmp = [int(m) for m in tmp]
-            self.hashes.append(tmp)
         self.allHashTrailZeros = []
         for i in self.hashes:
             a = i[0]
@@ -67,21 +74,23 @@ class FlajoletMartin:
                 self.trailingZeros.append(n)
             self.allHashTrailZeros.append(max(self.trailingZeros))
         r = self.average(self.allHashTrailZeros)
-        print('*****EXTENDED FM ALGORITHM with Average of all r`s*****\n')
+        print('\n*****EXTENDED FM ALGORITHM with Average of all r`s*****')
         print('avg r =',r)
         print('R(number of distinct elements) =',2**r)
 
     def extendedFMAlgorithmMedian(self):
         r = self.median(self.allHashTrailZeros)
-        print('*****EXTENDED FM ALGORITHM with median of all r`s*****\n')
+        print('\n*****EXTENDED FM ALGORITHM with median of all r`s*****')
         print('median r =',r)
-        print('R(number of distinct elements) =',2**r,'(~ =)',int(2**r)+1)
+        if int(2**r) == 2**r:
+            print('R(number of distinct elements) =',2**r)
+        else:
+            print('R(number of distinct elements) =',2**r,'(~= '+str(int(2**r)+1)+')')
 
     def combineApproach(self):
         if len(self.hashes) < 4:
-            print('combine approach is not possible due to less no of hashes. we wont be able to group them.')
+            print('\n*****combine approach is not possible due to less no of hashes. we wont be able to group them.*****')
             return
-        self.groupSize = int(input('enter the grouping size\n'))
         i = 0
         newAr = []
         while i< len(self.allHashTrailZeros):
@@ -90,19 +99,32 @@ class FlajoletMartin:
             i += self.groupSize
 
         r = self.median(newAr)
+        print('\n*****COMBINE APPROACH with making groups of size '+str(self.groupSize)+' and taking avg of each group and finally taking the median of all avg`s.*****')
         print('combined r =',r)
-        print('R =',2**r)
+        print('R(number of distinct elements) =',2**r)
 
-
-
-
-x = input('enter the data stream\n').strip().split()
-x = [int(m) for m in x]
+#x = input('enter the data stream\n').strip().split()
+#x = [int(m) for m in x]
 print('hash format = (ax+b)%c')
-print('enter a, b and c space seperated in the next line')
-abc = input().strip().split()
-object = FlajoletMartin(x,abc)
-object.runFMAlgorithm()
-object.extendedFMAlgorithm()
-object.extendedFMAlgorithmMedian()
-object.combineApproach()
+hashes = []
+no_of_hash = int(input('enter the number of hash you require?\n'))
+for i in range(no_of_hash):
+    if i+1 == 1:
+        tmp = input('enter the '+str(i+1)+'st hash coefficients\n' ).strip().split()
+        abc = []
+        abc.extend(tmp)
+    elif i+1 == 2:
+        tmp = input('enter the '+str(i+1)+'nd hash coefficients\n' ).strip().split()
+    elif i+1 == 3:
+        tmp = input('enter the '+str(i+1)+'rd hash coefficients\n' ).strip().split()
+    else:
+        tmp = input('enter the '+str(i+1)+'th hash coefficients\n' ).strip().split()
+    tmp = [int(m) for m in tmp]
+    hashes.append(tmp)
+
+#abc = input().strip().split()
+object = FlajoletMartin(abc,hashes)
+#object.runFMAlgorithm()
+#object.extendedFMAlgorithm()
+#object.extendedFMAlgorithmMedian()
+#object.combineApproach()
